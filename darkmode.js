406eb0c378548b5ac36f809bb2b41c183d3cecea
blu1115 <121748510+blu1115@users.noreply.github.com>
2023-01-09 05:18:52 -0600

    Add files via upload

---
+++ darkmode.js
@@ -0,0 +1,14 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode")
+}
+
+function forest() {
+    var element = document.body;
+    element.classList.toggle("forest")
+}
+
+function fantasy() {
+    var element = document.body;
+    element.classList.toggle("fantasy")
+}
